b76a971aff38dbac4fae02045c2fa20690c075cd
NaNathan13 <natewilson135@gmail.com>
2021-04-11 14:12:03 -0400

    created Wilson Web Development Starter Theme. Setup scripts to compile css and bundle project with exclusions. Copied everything over from previous work. Need to create short code elements that can be used as componenets

---
+++ README.md
@@ -0,0 +1,22 @@
+Installation
+---------------
+### Requirements
+
+- [Node.js](https://nodejs.org/)
+
+
+### Quick Start
+
+Clone or download this repository, rename theme, change `Wilson Web Development Starter Theme` to client name.
+
+
+### Setup
+
+Run `npm install` in terminal to download all dependencies
+
+
+### Available CLI commands
+
+- `gulp compile:sass` : Compiles ass sass files into a minified style.css in css/
+- `npm run bundle` : Zips all fipes with necessary exclusions
+
---
+++ footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Wilson_Web_Development
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wilson-web-development' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'wilson-web-development' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'wilson-web-development' ), 'wilson-web-development', '<a href="https://nathanwilsondev.com">Nathan J Wilson</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
