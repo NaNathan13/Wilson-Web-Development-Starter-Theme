c3e1dc0fecab359c3033df53491f9e9fc58784d1
NaNathan13 <natewilson135@gmail.com>
2021-04-11 17:57:22 -0400

    added contact and sytle guide page templates

---
+++ contact.php
@@ -0,0 +1,20 @@
+
+<?php
+/*
+ * Template Name: Contact Page
+ * description: Page template for contact page.
+  
+
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<h1>Contact Page</h1>
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
---
+++ styleGuide.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template Name: Style Guide
+ *
+ * Template for the contact page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Wilson_Web_Development
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<section>
+			<h1>Headline 1</h1>
+			<h2>Headline 2</h2>
+			<h3>Headline 3</h3>
+			<h4>Headline 4</h4>
+			<h5>Headline 5</h5>
+			<h6>Headling 6</h6>
+			<p>1 2 3 4 5 6 7 8 9</p>
+			<p>A B C D E F G H I J K L M N O P Q R S T U V W X Y Z</p>
+		</section>
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
